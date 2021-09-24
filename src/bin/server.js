98a3ee28eaa02151711dc3243e12c6877e378670
scalerjo <joseph.scalera1@gmail.com>
2021-09-24 17:20:32 +0000

    undo test

--- src/bin/server.js
+++ src/bin/server.js
@@ -7,15 +7,7 @@ const TABLE_SIZE = process.env.DHT_TABLESIZE || 1000000;
 const id_max = process.env.DHT_ID_MAX;
 const dht_data_dir = process.env.DHT_DATA_DIR;
 const obj_data_dir = process.env.OBJ_DATA_DIR;
-
-// const args = process.argv.slice(2)
-// const dht_port = args[1];
-// const obj_port = args[0];
-// const url = args[2] + ":" + args[1];
-// const id = args[3];
-
 const dht_port = process.env.DHT_PORT;
-console.log(dht_port);
 const obj_port = process.env.OBJ_PORT;
 const url = "http://" + process.env.HOST + ":" + dht_port;
 const id = process.env.DHT_ID;
